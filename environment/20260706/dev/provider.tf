5d980c33b0ed127c8e1fed12935ac55e9e2fee7f
M-S-A-11 <96737448+M-S-A-11@users.noreply.github.com>
2026-07-19 16:27:41 +0530

    Update provider.tf

--- environment/20260706/dev/provider.tf
+++ environment/20260706/dev/provider.tf
@@ -5,7 +5,13 @@ terraform {
       version = "4.76.0"
     }
   }
-  backend "azurerm" {}
+
+  backend "azurerm" {
+    resource_group_name   = "ADO_test_RG"
+    storage_account_name  = "adobackend321"
+    container_name        = "tfstate"          # must exist in the storage account
+    key                   = "terraform.tfstate"
+  }
 }
 
 provider "azurerm" {
